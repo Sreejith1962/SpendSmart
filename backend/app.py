7684bccb215800532e649cbe4c52de7a0273a9cf
Sreejith S <sreejithjnv1962@gmail.com>
2025-03-11 12:48:09 +0530

    requirements.txt for the render

--- backend/app.py
+++ backend/app.py
@@ -330,7 +330,9 @@ def calculate():
 
 
 
+
 if __name__ == '__main__':
-    with app.app_context():  
-        db.create_all() 
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
